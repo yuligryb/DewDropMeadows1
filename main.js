480877b05e778b77b4e2edafe00c5c5da71d6db0
yuligryb <118086373+yuligryb@users.noreply.github.com>
2025-05-01 04:39:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -221,7 +221,13 @@ const config = {
   width: 1920,
   height: 1080,
   scene: [MainMenu, FishingScene],
-  // ... other config settings
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  audio: {
+    disableWebAudio: false
+  }
 };
 
 new Phaser.Game(config);
